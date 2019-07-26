3ffeb73ffc5b4a091427bf83f0e676c0f32cd5f7
Nicholas Kastanos <nicholaskastanos@gmail.com>
2019-07-26 17:25:45 +0200

    One way data binding

--- app/routine-change-detector/src/app/tab2/tab2.page.ts
+++ app/routine-change-detector/src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular'
+
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
@@ -9,53 +11,53 @@ import { Geolocation } from '@ionic-native/geolocation/ngx';
 export class Tab2Page {
 
   private static _G = 9.80665
-  
-  /*
-  private setElementInnerHTML(element: any, text: String){
-    if(element == null || text == null){
-      console.log({
-        Error: "Element or Text is null or undefined", 
-        Element: element, 
-        Text: text
-      })
-      if(element != null || text == null){
-        this.setElementInnerHTML(element, "Null")
-      }
-    } else {
-      element.innerHTML = text
-    }
+  acceleration: number
+  gyroscope: number
+  magnetometer: number
+  geoData = {
+    latitude: 0,
+    longitude: 0,
+    accuracy: 0,
+    altitude: 0,
+    altitudeAccuracy: 0,
+    heading: 0,
+    speed: 0,
+    timestamp: 0
   }
-  */
 
-  constructor(private geolocation: Geolocation) {
-    document.addEventListener("deviceready", onDeviceReady, false);
-    function onDeviceReady() {
-     
-      function geolocationSuccess(response){
-        console.log(response)
-      }
-      function geolocationError(error){
-        console.log(error)
-      }
+  constructor(private navCtrl: NavController, private geolocation: Geolocation) {
+
+    // Add Event Listener for Acceleration and Gyroscope
+    window.addEventListener("devicemotion", (event) => {
+      this.acceleration = Math.sqrt(Math.pow(event.accelerationIncludingGravity.x,2)+Math.pow(event.accelerationIncludingGravity.y,2)+Math.pow(event.accelerationIncludingGravity.z,2))/Tab2Page._G
+      this.gyroscope = Math.sqrt(Math.pow(event.rotationRate.alpha,2)+Math.pow(event.rotationRate.beta,2)+Math.pow(event.rotationRate.gamma,2))*(Math.PI/180)
+    }, true)
+
+    // Add Event Listener for Magnetometer
+    window.addEventListener("deviceorientation", (event) => {
+      this.magnetometer = Math.sqrt(Math.pow(event.alpha,2) + Math.pow(event.beta,2) + Math.pow(event.gamma,2))
+    }, true)
+
+    document.addEventListener("deviceready", () => {
       let geolocationOptions = { maximumAge: 25, timeout: 500, enableHighAccuracy: true };
-
-      let geoWatchId = navigator.geolocation.watchPosition(geolocationSuccess, geolocationError, geolocationOptions)
-      
-      window.addEventListener("devicemotion", function(event) {
-        // process event.alpha, event.beta and event.gamma
-        let acc = this.document.getElementById("lbl:acc")
-        acc.innerHTML = (Math.sqrt(Math.pow(event.accelerationIncludingGravity.x,2)+Math.pow(event.accelerationIncludingGravity.y,2)+Math.pow(event.accelerationIncludingGravity.z,2))/Tab2Page._G).toPrecision(6)
-        let gyr = this.document.getElementById("lbl:gyro")
-        gyr.innerHTML = (Math.sqrt(Math.pow(event.rotationRate.alpha,2)+Math.pow(event.rotationRate.beta,2)+Math.pow(event.rotationRate.gamma,2))*(Math.PI/180)).toPrecision(6)
-      }, true)
-    
-      window.addEventListener("deviceorientation", function(event) {
-        let mag = this.document.getElementById("lbl:magn")
-        mag.innerHTML = Math.sqrt(Math.pow(event.alpha,2) + Math.pow(event.beta,2) + Math.pow(event.gamma,2)).toPrecision(6)
-      }, true)
-    }
-    
-    
+      let geoWatchId = navigator.geolocation.watchPosition( 
+        // GEOLOCATION SUCCESS
+        (response) => {
+        console.log(response)
+        this.geoData.latitude = response.coords.latitude
+        this.geoData.longitude = response.coords.longitude
+        this.geoData.accuracy = response.coords.accuracy
+        this.geoData.altitude = response.coords.altitude
+        this.geoData.altitudeAccuracy = response.coords.altitudeAccuracy
+        this.geoData.heading = response.coords.heading
+        this.geoData.speed = response.coords.speed
+        this.geoData.timestamp = response.timestamp
+      }, 
+      //  GEOLOCATION ERROR
+      (error) => {
+        console.log(error)
+      }, 
+      geolocationOptions)
+    }, false)    
   }
-
 }
