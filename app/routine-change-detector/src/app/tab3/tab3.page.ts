1ad9d66c300d212ebcffaca91916de954fb14d18
Anita <demellokochanita@gmail.com>
2019-08-19 10:10:54 +0200

    Button

--- app/routine-change-detector/src/app/tab3/tab3.page.ts
+++ app/routine-change-detector/src/app/tab3/tab3.page.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
 
-import Meyda from 'meyda/dist/web/meyda'
-declare var audioinput: any
-
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -10,82 +7,9 @@ declare var audioinput: any
 })
 export class Tab3Page {
 
-  recording = false
+  constructor() {}
 
-  constructor() {
-    audioinput.checkMicrophonePermission(function(hasPermission) {
-      if (hasPermission) {
-        console.log("We already have permission to record.");
-            // startCapture();
-      } 
-      else {	        
-            // Ask the user for permission to access the microphone
-        audioinput.getMicrophonePermission(function(hasPermission, message) {
-          if (hasPermission) {
-            console.log("User granted us permission to record.");
-                    // startCapture();
-          } else {
-            console.warn("User denied permission to record.");
-          }
-        });
-      }
-    });
-  }
-
-  private readonly _mfccBuffer = 1024
-  private _prevBuffer = new Array(this._mfccBuffer).fill(0);
-
-  private _audioInputEvent = (event) => {
-    // console.log( "Audio data received: " + event.data.length + " samples" )
-    // console.log(event)
-    let audData: number[]
-    audData = Array.from(event.data)
-    audData.push(audData[audData.length - 1])
-
-    let normFactor = 0
-    audData.forEach(element => {
-      if (Math.abs(element) > normFactor){
-        normFactor = Math.abs(element)
-      }
-    });
-    let normData: number[]
-    normData = []
-    audData.forEach(element => {
-      normData.push(element/normFactor)
-    });
-
-    // console.log({'audData':audData, 'normData': normData, 'normFactor': normFactor})
-
-    Meyda.bufferSize = this._mfccBuffer
-    Meyda.melBands = 40
-    Meyda.sampleRate = 22050
-    console.log(Meyda.extract('mfcc', normData, this._prevBuffer))
-    this._prevBuffer = normData
-  }
-  private _audioErrorEvent = (error) => {
-    console.log(error)
-    this.stopRecord()
-  }
-
-  captureCfg = {
-    sampleRate: audioinput.SAMPLERATE.CD_HALF_22050Hz ,//16000,
-    bufferSize: this._mfccBuffer,
-    channels: audioinput.CHANNELS.MONO, // 1
-    format: audioinput.FORMAT.PCM_16BIT, 
-    audioSourceType: audioinput.AUDIOSOURCE_TYPE.DEFAULT,
-    normalizationFactor: 32767.0/3.0 
-  }
-
-  public startRecord(){
-    this.recording = true
-    audioinput.start(this.captureCfg)
-    window.addEventListener('audioinput', this._audioInputEvent, false)
-    window.addEventListener('audioinputerror', this._audioErrorEvent, false)
-  }
-  public stopRecord(){
-    this.recording = false
-    audioinput.stop()
-    window.removeEventListener('audioinput', this._audioInputEvent, false)
-    window.removeEventListener('audioinputerror', this._audioErrorEvent, false)
+  sendData(){
+    console.log("not implemented yet")
   }
 }
