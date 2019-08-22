123217a064e53784d15da0eaea0842544ef32807
Anita <demellokochanita@gmail.com>
2019-08-22 19:57:54 +0200

    Remove confusion matrix plotting function

--- ml/mlp/MLP.py
+++ ml/mlp/MLP.py
@@ -8,63 +8,6 @@ import argparse
 import matplotlib.pyplot as plt
 from sklearn.utils.multiclass import unique_labels
 
-def plot_confusion_matrix(y_true, y_pred, classes,
-                          normalize=False,
-                          title=None,
-                          cmap=plt.cm.Blues):
-    """
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
-    """
-    if not title:
-        if normalize:
-            title = 'Normalized confusion matrix'
-        else:
-            title = 'Confusion matrix, without normalization'
-
-    # Compute confusion matrix
-    cm = multilabel_confusion_matrix(y_true, y_pred)
-    print(cm.shape)
-    new_cm = np.reshape(cm, [-1, 4])
-    print(new_cm)
-    # Only use the labels that appear in the data
-#     classes = classes[unique_labels(y_true, y_pred)]
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        print("Normalized confusion matrix")
-    else:
-        print('Confusion matrix, without normalization')
-
-    print(cm)
-
-    fig, ax = plt.subplots()
-    plt.imsave('test.png' ,new_cm, cmap=cmap)
-#     ax.figure.colorbar(im, ax=ax)
-#     # We want to show all ticks...
-#     ax.set(xticks=np.arange(cm.shape[1]),
-#            yticks=np.arange(cm.shape[0]),
-#            # ... and label them with the respective list entries
-#            xticklabels=classes, yticklabels=classes,
-#            title=title,
-#            ylabel='True label',
-#            xlabel='Predicted label')
-
-#     # Rotate the tick labels and set their alignment.
-#     plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
-#              rotation_mode="anchor")
-
-#     # Loop over data dimensions and create text annotations.
-#     fmt = '.2f' if normalize else 'd'
-#     thresh = cm.max() / 2.
-#     for i in range(cm.shape[0]):
-#         for j in range(cm.shape[1]):
-#             ax.text(j, i, format(cm[i, j], fmt),
-#                     ha="center", va="center",
-#                     color="white" if cm[i, j] > thresh else "black")
-#     fig.tight_layout()
-#     plt.savefig(title + '.png')
-#     return ax
-
 def main(training_dir, checkpointdir, training_files, train_percent, initial_learning_rate, epochs, batch_size, weighting_action_1, weighting_action_2, weighting_loc, weighting_phone, hidden_1, hidden_2, hidden_3, hidden_4, hidden_5, hidden_6, action_1_layer_1, action_2_layer_1, n_input, n_labels, n_output_action_1, n_output_action_2, n_output_loc, n_output_phone, regulariser_rate):
 
         f = open("output.log", "w+")
